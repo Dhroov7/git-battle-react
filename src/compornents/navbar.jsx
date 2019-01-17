245b8ad1c5280667a781158d6ef435a8aaed6439
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 00:08:55 +0530

    modify navbar

--- src/compornents/navbar.jsx
+++ src/compornents/navbar.jsx
@@ -4,12 +4,12 @@ class Navbar extends Component {
     render() {
         return (
             <React.Fragment>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Git Battle</a>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                            <a class="nav-item nav-link" href="#">Top Gitters</a>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Git Battle</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#">Top Gitters</a>
                         </div>
                     </div>
                 </nav>
