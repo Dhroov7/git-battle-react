c5d9d5360d4806432624a68ff911616d2a1c8557
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 00:07:28 +0530

    added gitterTwo component

---
+++ src/compornents/gitterOne.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class GitterOne extends Component {
+    
+    constructor(){
+        super();
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            query: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({query: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addGitterOne(this.state.query)
+    }
+
+    render() { 
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="gitterOne">Gitter Two:</label>
+                    <input type="text" id="gitterOne" className="form-control" placeholder="Enter Your Github Username" onChange={this.handleChange} />
+                </div>
+                <input type="submit" className="btn btn-secondary" value="Submit"></input>
+            </form>
+        );
+    }
+}
+ 
+export default GitterOne;
