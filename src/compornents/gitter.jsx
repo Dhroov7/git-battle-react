0d62a677f14509bbd6586889e43b5514f05d7f5b
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 03:56:49 +0530

    some ui layout changes

--- src/compornents/gitter.jsx
+++ src/compornents/gitter.jsx
@@ -5,17 +5,30 @@ import GitterTwo from './gitterTwo'
 class Gitter extends Component {
     state = {}
     render() {
+        let battleButton
+        if (this.props.battleIsOn) {
+            battleButton = <button onClick={this.props.battle} className="btn btn-secondary">Battle</button>
+        }
         return (
-            <div className="row m-5">
-                <div className="col-2"></div>
-                <div className="col">
-                    <GitterOne addGitterOne={this.props.addGitterOne} />
+            <div>
+                <div className="row m-5">
+                    <div className="col-2"></div>
+                    <div className="col">
+                        <GitterOne addGitterOne={this.props.addGitterOne} battleIsOn={this.props.battleIsOn} gitterOne={this.props.gitterOne} />
+                    </div>
+                    <div className="col-2"></div>
+                    <div className="col">
+                        <GitterTwo addGitterTwo={this.props.addGitterTwo} battleIsOn={this.props.battleIsOn} gitterTwo={this.props.gitterTwo} />
+                    </div>
+                    <div className="col-2"></div>
                 </div>
-                <div className="col-2"></div>
-                <div className="col">
-                    <GitterTwo addGitterTwo={this.props.addGitterTwo} />
+                <div className="row m-3">
+                    <div className="col-5"></div>
+                    <div className="col-2">
+                        {battleButton}
+                    </div>
+                    <div className="col-5"></div>
                 </div>
-                <div className="col-2"></div>
             </div>
         );
     }
