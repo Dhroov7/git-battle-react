fc9caa2dca3360ec75d9d7488ee6ec2a22924008
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 00:07:44 +0530

    added gitter component

---
+++ src/compornents/gitter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import GitterOne from './gitterOne'
+import GitterTwo from './gitterTwo'
+
+class Gitter extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="row m-5">
+                <div className="col-2"></div>
+                <div className="col">
+                    <GitterOne addGitterOne={this.props.addGitterOne} />
+                </div>
+                <div className="col-2"></div>
+                <div className="col">
+                    <GitterTwo addGitterTwo={this.props.addGitterTwo} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Gitter;
