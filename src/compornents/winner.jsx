9e8f398f4e135f5838c16ede7bedf3a5a03e3bfd
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 23:48:27 +0530

    added new style

--- src/compornents/winner.jsx
+++ src/compornents/winner.jsx
@@ -8,6 +8,24 @@ class Winner extends Component {
             borderRadius: '50%',
             width: '70%'
         }
+
+        this.h5Style = {
+            color: 'lightgrey',
+            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+            fontSize: '20px'
+        }
+
+        this.h2Style = {
+            color: 'lightgrey',
+            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+            fontSize: '30px'
+        }
+
+        this.h4Style = {
+            color: 'lightgrey',
+            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+            fontSize: '25px'
+        }
     }
 
     render() {
@@ -18,10 +36,10 @@ class Winner extends Component {
                     <div className="col">
                         <div className="row">
                             <div className="col-12 mb-2">
-                                <h2 className="pl-5">Winner!</h2>
+                                <h2 style={this.h2Style} className="pl-5">Winner!</h2>
                             </div>
                             <div className="col-12 mb-2">
-                                <h4 className="pl-5">Score: {this.props.winner.score}</h4>
+                                <h4 style={this.h4Style} className="pl-5">Score: {this.props.winner.score}</h4>
                             </div>
                             <div className="col-12">
                                 <div className="pl-3">
@@ -29,16 +47,16 @@ class Winner extends Component {
                                 </div>
                             </div>
                             <div className="col-12 mt-2">
-                                <h2>{this.props.winner.name}</h2>
+                                <h2 style={this.h2Style}>{this.props.winner.name}</h2>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Followers: {this.props.winner.followers}</h5>
+                                <h5 style={this.h5Style}>Followers: {this.props.winner.followers}</h5>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Public Gists: {this.props.winner.public_gists}</h5>
+                                <h5 style={this.h5Style}>Public Gists: {this.props.winner.public_gists}</h5>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Public Repos: {this.props.winner.public_repos}</h5>
+                                <h5 style={this.h5Style}>Public Repos: {this.props.winner.public_repos}</h5>
                             </div>
                         </div>
                     </div>
@@ -46,25 +64,25 @@ class Winner extends Component {
                     <div className="col">
                         <div className="row">
                             <div className="col-12 mb-2">
-                                <h2 className="pl-5">Loser!</h2>
+                                <h2 style={this.h2Style} className="pl-5">Loser!</h2>
                             </div>
                             <div className="col-12 mb-2">
-                                <h4 className="pl-5">Score: {this.props.loser.score}</h4>
+                                <h4 style={this.h4Style} className="pl-5">Score: {this.props.loser.score}</h4>
                             </div>
                             <div className="col-12">
                                 <img style={this.style} src={this.props.loser.avatar_url} alt="Loser"></img>
                             </div>
                             <div className="col-12 mt-2">
-                                <h2>{this.props.loser.name}</h2>
+                                <h2 style={this.h2Style}>{this.props.loser.name}</h2>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Followers: {this.props.loser.followers}</h5>
+                                <h5 style={this.h5Style}>Followers: {this.props.loser.followers}</h5>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Public Gists: {this.props.loser.public_gists}</h5>
+                                <h5 style={this.h5Style}>Public Gists: {this.props.loser.public_gists}</h5>
                             </div>
                             <div className="col-12 mt-2">
-                                <h5>Public Repos: {this.props.loser.public_repos}</h5>
+                                <h5 style={this.h5Style}>Public Repos: {this.props.loser.public_repos}</h5>
                             </div>
                         </div>
                     </div>
