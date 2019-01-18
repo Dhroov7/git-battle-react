9e78433f8f0e19277afb651a054d8a94291e469c
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-01-18 23:49:07 +0530

    added new style

--- src/compornents/gitterOne.jsx
+++ src/compornents/gitterOne.jsx
@@ -13,6 +13,27 @@ class GitterOne extends Component {
             borderRadius: '50%',
             width: '70%'
         }
+
+        this.inputBoxStyle = {
+            borderRadius: '25px',
+            backgroundColor: '#C0C0C0'
+        }
+
+        this.buttonStyle = {
+            borderRadius: '25px'
+        }
+
+        this.labelStyle = {
+            color: 'lightgrey',
+            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+            fontSize: '20px'
+        }
+
+        this.gitterNameStyle = {
+            color: 'lightgrey',
+            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+            fontSize: '30px'
+        }
     }
 
     handleChange = (e) => {
@@ -32,7 +53,7 @@ class GitterOne extends Component {
                         <img style={this.style} src={this.props.gitterOne.avatar_url} alt="contender One"></img>
                     </div>
                     <div className="col-12 mt-2">
-                      <h2>{this.props.gitterOne.name}</h2>
+                        <h2 style={this.gitterNameStyle}>{this.props.gitterOne.name}</h2>
                     </div>
                 </div>
             )
@@ -40,10 +61,10 @@ class GitterOne extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
-                    <label htmlFor="gitterOne">Contender One:</label>
-                    <input type="text" id="gitterOne" className="form-control" placeholder="Enter Your Github Username" onChange={this.handleChange} />
+                    <label htmlFor="gitterOne" style={this.labelStyle}>Contender One:</label>
+                    <input style={this.inputBoxStyle} type="text" className="form-control" id="gitterOne" placeholder="Enter Your Github Username" onChange={this.handleChange} />
                 </div>
-                <input type="submit" className="btn btn-secondary" value="Submit"></input>
+                <input type="submit" style={this.buttonStyle} className="btn btn-secondary" value="Submit"></input>
             </form>
         );
     }
